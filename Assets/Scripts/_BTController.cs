2124014e2ea8592f1c86eabddf485497d85a2aa9
Elevista <sunnyholic@sunnyholic.com>
2015-05-09 17:14:51 +0900

    prefab 업데이트

--- Assets/Scripts/_BTController.cs
+++ Assets/Scripts/_BTController.cs
@@ -9,18 +9,13 @@ public class _BTController : MonoBehaviour {
 	private bool isRightDown = false;
 	private bool isFireDown=false;
 	private bool isJumpDown=false;
-	CharacterMotor motor;
-	GameObject player;
-	void Start(){
-		player=GameObject.Find("First Person Controller");
-		motor=player.GetComponent<CharacterMotor>();
-	}
+	public GameObject player;
 	void Update(){
 		player.SendMessage ("MoveForward",isForDown);
 		player.SendMessage ("MoveBack",isBackDown);
 		player.SendMessage ("MoveLeft",isLeftDown);
 		player.SendMessage ("MoveRight",isRightDown);
 		player.SendMessage ("Fire", isFireDown);
-		motor.inputJump = isJumpDown;
+		player.SendMessage ("Jump",isJumpDown);
 	}
 }
