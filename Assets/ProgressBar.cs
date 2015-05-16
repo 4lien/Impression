2c68f8237cc9305c42f5f292108040c11c081f8a
Elevista <sunnyholic@sunnyholic.com>
2015-05-16 20:13:40 +0900

    HP,총알 표시 & 재장전

---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+	public float percent=0f;
+	RawImage gage;
+	RawImage bg;
+	// Use this for initialization
+	void Start () {
+		gage = transform.FindChild ("gage").GetComponent<RawImage> ();
+		bg =  GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (percent >= 1f)
+			percent = 1f;
+		Vector3 t = gage.transform.localScale;
+		t.x = percent;
+		gage.transform.localScale = t;
+	}
+	public void on(){	//활성화
+		this.enabled = true;
+		transform.localScale = new Vector3 (1f, 1f, 1f);
+	}
+	public void off(){ //비활성화
+		this.enabled = false;
+		transform.localScale = new Vector3 (0f, 0f, 0f);
+	}
+
+}
--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent (typeof (CharacterMotor))]
 public class FirstPersonController : GameObjectParent {
 	public Animator Ani;
-	private Vector3 moveDirectionhitRayVector3.zero;
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
-	public float hitRange= 5F;
+	public float hitRange= 5.0F;
+	public float reloadTime=5.0F;
+	public float waitJumpTime=2.0f;
+	public int curAmmo = 30;
+	public int maxAmmo= 30;
+	public int remainAmmo=120;
+
 	public AudioClip fireSnd;
 	public AudioClip hitSnd;
 	public AudioClip hitSnd2;
 	public AudioClip jumpSnd;
+	public AudioClip noAmmoSnd;
+	public AudioClip reloadSnd;
 	public GameObject muzzleFlash;
 	public GameObject hitEffect;
 	public GameObject gun;
@@ -21,6 +29,9 @@ public class FirstPersonController : GameObjectParent {
 	public GameObject camPos;
 	public GameObject bulletHole;
 	public LayerMask ignoreRaycast;
+	public Text HP;
+	public Text AMMO;
+	public ProgressBar ReloadBar;
 	CharacterMotor motor;
 	CharacterController con;
 
@@ -33,6 +44,8 @@ public class FirstPersonController : GameObjectParent {
 	int arrayLength=30;
 	const float gunSpeed = 8f;
 	float fireDelay=0f;
+	float reloadTimer=0f;
+	float jumpTimer=0f;
 	CharacterController controller;
 	const float sin45 = 0.7071067811865475F;
 		// Use this for initialization
@@ -46,6 +59,7 @@ public class FirstPersonController : GameObjectParent {
 		damage = 80;
 		hp = 100;
 		con = GetComponent < CharacterController >();
+		AMMO.text = curAmmo + "/" + remainAmmo;
 	}
 
 
@@ -54,10 +68,22 @@ public class FirstPersonController : GameObjectParent {
 	//RaycastHit hit2;
 	int lastBullet = 0;
 
+	bool noammoFlag=false;
 	void gunFire(){
 		if (fireDelay == 0) {	//빵하고 쏨.
+			if(curAmmo<=0){	//총알이 없으면
+				if(noammoFlag){
+					gun.audio.PlayOneShot(noAmmoSnd);
+					noammoFlag=false;
+				}
+				return;
+			}
+			curAmmo--;
+			if(curAmmo<=0)noammoFlag=true;
+			reloadStop();
 			fireDelay += Time.deltaTime*gunSpeed;
-			AudioSource.PlayClipAtPoint (fireSnd, muzzlePosition.transform.position);
+			//AudioSource.PlayClipAtPoint (fireSnd, muzzlePosition.transform.position);
+			muzzlePosition.audio.PlayOneShot(fireSnd);
 			Instantiate(muzzleFlash,muzzlePosition.transform.position,transform.rotation);
 			Physics.Raycast (camera.transform.position, camera.transform.forward,out hitRay, 100,~ignoreRaycast);
 			//Physics.SphereCast(camera.transform.position,hitRange, camera.transform.forward,out hit2, 100,dynamicLayer);
@@ -85,14 +111,17 @@ public class FirstPersonController : GameObjectParent {
 				}
 			}
 		}
+		AMMO.text = curAmmo + "/" + remainAmmo;
 	}
 
 
 
 	// Update is called once per frame
 	void Update () {
-		if (hp <= 0)
+		if (hp <= 0) {
+			HP.text="HP:0";
 			return;
+		}
 		Debug.DrawRay(camera.transform.position, camera.transform.forward*100, Color.red);
 
 		if (fireDelay > 0) {
@@ -173,6 +202,7 @@ public class FirstPersonController : GameObjectParent {
 	}
 	public void hit(float Damage){
 		hp -= Damage;
+		HP.text = "HP:" + hp;
 		AudioSource.PlayClipAtPoint(hitSnd2,transform.position,0.2f);
 		if (hp < 0)
 			dead ();
@@ -212,16 +242,66 @@ public class FirstPersonController : GameObjectParent {
 	public void Fire(bool val)	{
 		shooting=val;
 	}
+	public void reloadStart(){
+	}
+	public void reloadStop(){
+		ReloadBar.percent=0f;
+		ReloadBar.off ();
+		reloadTimer=0f;
+		gun.audio.Stop();
+		reloadSndFlag=true;
+	}
+	public void reload(){
+		reloadStop ();
+
+		int needAmmo = maxAmmo - curAmmo;
+		if (remainAmmo >= needAmmo) {
+			curAmmo += needAmmo;
+			remainAmmo -= needAmmo;
+		} else {
+			curAmmo+=remainAmmo;
+			remainAmmo=0;
+		}
+		noammoFlag = false;
+
+		AMMO.text = curAmmo + "/" + remainAmmo;
+		reloadFlag = true;
+	}
+	bool reloadFlag=false;
+	bool reloadSndFlag=true;
 	public void Jump(bool val){
 		if (hp <= 0)
 			return;
-		if (val)	//다운
-			motor.inputJump = false;
-		if(jump&&!val){	//업
-			if(Physics.Raycast(transform.position, -Vector3.up,0.01f)){	//땅에 붙어있을때만
-				AudioSource.PlayClipAtPoint(jumpSnd,transform.position);	//기합
+		if (val) {	//다운
+			jumpTimer+=Time.deltaTime;
+			if(remainAmmo>0&&curAmmo<maxAmmo){
+				ReloadBar.on (); //리로드바 키고
+				if(reloadSndFlag){
+					gun.audio.PlayOneShot(reloadSnd);
+					reloadSndFlag=false;
+				}
+
+				//리로드 안된 상태면 리로드 시작
+				if(reloadTimer<reloadTime){
+					reloadTimer+=Time.deltaTime;
+					ReloadBar.percent=reloadTimer/reloadTime;
+				}else{
+					reloadTimer=reloadTime;
+					reload ();
+				}
 			}
-			motor.inputJump = true;
+			motor.inputJump = false;
+		}
+		if(jump&&!val){	//업
+			//누른지 일정 시간이 안지났을때
+			if(jumpTimer<=waitJumpTime){
+				if(Physics.Raycast(transform.position, -Vector3.up,0.01f)){	//땅에 붙어있을때만
+					audio.PlayOneShot(jumpSnd);	//기합
+				}
+				motor.inputJump = true; //점프
+			}
+			reloadStop ();
+			jumpTimer=0f;
 		}
 		jump = val;
 	}
