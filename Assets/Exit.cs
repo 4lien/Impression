95ad1cadbb2bdad66e270c44da6d2747225cb5c3
Elevista <sunnyholic@sunnyholic.com>
2015-05-26 19:55:25 +0900

    클리어 출구 생성

---
+++ Assets/Exit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+	public GameObject player;
+	public GameObject info;
+	void OnTriggerStay(Collider p) {	//범위안에 플레이어 포착
+		if(player.transform!=p.transform)	//플레이어가 아니거나 죽었으면 안함
+			return;
+		info.SetActive (true);
+		StartCoroutine (destroy());
+	}
+	
+	IEnumerator destroy(){
+		yield return new WaitForSeconds(5f);
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+}
