aa5ff6c4593eaf9615b2508bd700e3b6ad03303f
4lien <sunnyholic@sunnyholic.com>
2015-05-27 17:31:46 +0900

    멀티 총쏘는거 전까지 구현
    Unity Android Pro가 있어야 폰에서 가능..

---
+++ Assets/Scripts/NetWorkManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetWorkManager : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		connect ();
+		
+	}
+	void connect(){
+		//PhotonNetwork.offlineMode = true;
+		PhotonNetwork.ConnectUsingSettings ("0.0.1");
+		
+	}
+	
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+	
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom ();
+	}
+	
+	void OnPhotonRandomJoinFailed(){
+		PhotonNetwork.CreateRoom (null);
+	}
+	void OnJoinedRoom(){
+		Debug.Log ("OnJoinedRoom");
+		SpawnMyPlayer ();
+	}
+	
+	void SpawnMyPlayer(){
+		GameObject GameSet=(GameObject)PhotonNetwork.Instantiate ("GameSet",Vector3.zero,Quaternion.identity,0);
+		GameObject OSD=GameSet.transform.FindChild("OSD").gameObject;
+		GameObject _KeyController=GameSet.transform.FindChild("_KeyController").gameObject;
+		GameObject Player=GameSet.transform.FindChild("Player").gameObject;
+		GameObject camera=Player.transform.FindChild("Main Camera").gameObject;
+		GameObject EventSystem=GameSet.transform.FindChild("EventSystem").gameObject;
+		EventSystem.SetActive (true);
+		_KeyController.SetActive (true);
+		OSD.SetActive (true);
+		camera.SetActive (true);
+		((MonoBehaviour)Player.GetComponent<FirstPersonController> ()).enabled = true;
+		((MonoBehaviour)Player.GetComponent<CharacterMotor> ()).enabled = true;
+	}
+}
---
+++ Assets/myNetworkPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myNetworkPlayer : Photon.MonoBehaviour
+{
+	private Vector3 correctPlayerPos = Vector3.zero; // We lerp towards this
+	private Quaternion correctPlayerRot = Quaternion.identity; // We lerp towards this
+	private float runDirect, speed, lookDegree;
+	private bool dead;
+	GameObject player;
+	Animator Ani;
+	void Start(){
+		player = transform.FindChild ("Player").gameObject;
+		Ani = player.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (!photonView.isMine)
+		{
+			player.transform.position = Vector3.Lerp(player.transform.position, this.correctPlayerPos, 0.5f);
+			player.transform.rotation = Quaternion.Lerp(player.transform.rotation, this.correctPlayerRot, 0.5f);
+			Ani.SetFloat ("runDirect",Mathf.Lerp(Ani.GetFloat("runDirect"),runDirect,0.5f));
+			Ani.SetFloat ("speed",Mathf.Lerp(Ani.GetFloat("speed"),speed,0.5f));
+			Ani.SetFloat ("lookDegree",Mathf.Lerp(Ani.GetFloat("lookDegree"),lookDegree,0.5f));
+			Ani.SetBool ("dead",dead);
+		}
+	}
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(player.transform.position);
+			stream.SendNext(player.transform.rotation);
+			stream.SendNext(Ani.GetFloat("runDirect"));
+			stream.SendNext(Ani.GetFloat("speed"));
+			stream.SendNext(Ani.GetFloat("lookDegree"));
+			stream.SendNext(Ani.GetBool("dead"));
+			
+			/*FirstPersonController myC = transform.Find("Player").gameObject.GetComponent<FirstPersonController>();
+			stream.SendNext((int)myC._characterState);*/
+		}
+		else
+		{
+			// Network player, receive data
+			this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+			this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+			runDirect=(float)stream.ReceiveNext();
+			speed=(float)stream.ReceiveNext();
+			lookDegree=(float)stream.ReceiveNext();
+			dead=(bool)stream.ReceiveNext();
+			/*myThirdPersonController myC = GetComponent<myThirdPersonController>();
+			myC._characterState = (CharacterState)stream.ReceiveNext();*/
+		}
+	}
+}
